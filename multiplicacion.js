2de0dbb662a7aefab38cf9eaf7410e6d5aeaea47
argprognc <nicochavez.ing@gmail.com>
2023-05-27 10:52:44 -0300

    agregué multiplicacion

--- multiplicacion.js
+++ multiplicacion.js
@@ -1,4 +1,9 @@
+// Ejercicio 1 TP 0
+
+let n1 = 2;
 let n2 = 17;
 let n3 = 6;
-let multiplicacion = 2 * 17 * 6;
+let multiplicacion = n1 * n2 * n3;
 console.log(multiplicacion);
+
+//console.log(n1 * n2 * n3);
