26c1f17a5c4947afbb642633ae64c72c9ca7bc89
argprognc <nicochavez.ing@gmail.com>
2023-05-27 11:06:59 -0300

    corrige areaRectangulo

--- areaRectangulo.js
+++ areaRectangulo.js
@@ -2,9 +2,10 @@
 
 let base = 5;
 let altura = 3;
-let area = base * altura
+let area = base * altura;
 console.log(area);
+
 base = 12.5;
 altura = 5.3;
-//console.log(area);
-console.log(base * altura);
+area = base * altura;
+console.log(area);
