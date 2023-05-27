dd7e67e6ccbe59bed234395068c6a59f103a6c9e
argprognc <nicochavez.ing@gmail.com>
2023-05-27 11:12:28 -0300

    corrige conversionGrados

--- conversionGrados.js
+++ conversionGrados.js
@@ -5,5 +5,5 @@ let celsius = (fahrenheit - 32)*5/9;
 console.log(celsius);
 
 fahrenheit = 75;
-//console.log(celsius);
-console.log((fahrenheit - 32)*5/9);
+celsius = (fahrenheit - 32)*5/9;
+console.log(celsius);
