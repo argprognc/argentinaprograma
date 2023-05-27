48cdea560ec8299b876049a97fbccdbd34ef116b
argprognc <nicochavez.ing@gmail.com>
2023-05-27 11:13:35 -0300

    corrige promedio3

--- promedio3.js
+++ promedio3.js
@@ -3,17 +3,17 @@
 let n1 = 5;
 let n2 = 7;
 let n3 = 9;
-let promedio = (n1 + n2 + n3)/3
+let promedio = (n1 + n2 + n3)/3;
 console.log(promedio);
 
 n1 = -2;
 n2 = -3;
 n3= -8;
-promedio = (n1 + n2 + n3)/3
-console.log(promedio)
+promedio = (n1 + n2 + n3)/3;
+console.log(promedio);
 
 n1 = -1.2;
 n2 = 5.3;
 n3= -3.3;
-promedio = (n1 + n2 + n3)/3
-console.log(promedio)
+promedio = (n1 + n2 + n3)/3;
+console.log(promedio);
