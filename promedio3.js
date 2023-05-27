26678974ccdffa80c925cfc85d1ef929d8f4f58f
argprognc <nicochavez.ing@gmail.com>
2023-05-27 10:54:32 -0300

    agregué promedio

---
+++ promedio3.js
@@ -0,0 +1,17 @@
+// Ejercicio 4 TP 0
+
+let n1 = 5;
+let n2 = 7;
+let n3 = 9;
+let promedio = (n1 + n2 + n3)/3
+console.log(promedio);
+
+n1 = -2;
+n2 = -3;
+n3= -8;
+console.log((n1 + n2 + n3)/3)
+
+n1 = -1.2;
+n2 = 5.3;
+n3= -3.3;
+console.log((n1 + n2 + n3)/3)
